1eeb46f4fa964466d348e92d573272b1e6f35b26
Udayan Prabir Sinha <udayan.sinha@outlook.com>
2023-08-28 17:19:30 +0200

    Add ioctl() support to misc. character driver

--- linux-kernel/examples/misc-char-drv/misc-char-drv.c
+++ linux-kernel/examples/misc-char-drv/misc-char-drv.c
@@ -1,25 +1,31 @@
 /*
  * misc-char-drv.c: Misc. Character Device Driver
- *		    This driver provides a random number value on reads.
+		    Kernel module for study/experiments
  * Author: Udayan Prabir Sinha
  * Date: 2023-08-21
  */
 
-#include <linux/module.h>
-#include <linux/init.h>
-#include <linux/printk.h>
-#include <linux/fs.h>
-#include <linux/miscdevice.h>
-#include <linux/random.h>
+#include <linux/module.h>	/* module_*(), MODULE_*() */
+#include <linux/init.h>		/* __init, __exit */
+#include <linux/printk.h>	/* printk() and its variants */
+#include <linux/fs.h>		/* struct file, file_operations, inode */
+#include <linux/slab.h>		/* memory allocation APIs */
+#include <linux/ioctl.h>	/* ioctl() utilities */
+#include <linux/miscdevice.h>	/* misc char-drv APIs */
+#include <linux/random.h>	/* random-number APIs */
+#include <linux/panic.h>	/* panic() */
 
 #define DEV_NAME "misc-char-drv"		/* name under /dev */
 
-static u64 rand_num_buf = ~0;
-static u32 rand_num_size = 4;	/* number of bytes */
+/* ioctl() commands */
+#define MISC_CHAR_DRV_BASE	'X'
+#define MISC_CHAR_DRV_PANIC	_IO(MISC_CHAR_DRV_BASE, 0)
+
+static void *rand_num_buf = NULL;
 
 static int misc_chardrv_open(struct inode *inode, struct file *filep)
 {
-	pr_devel("open() called. Major: %u, Minor: %u\n",
+	pr_debug("open() called. Major: %u, Minor: %u\n",
 		imajor(inode), iminor(inode));
 
 	return 0;
@@ -27,7 +33,7 @@ static int misc_chardrv_open(struct inode *inode, struct file *filep)
 
 static int misc_chardrv_release(struct inode *inode, struct file *filep)
 {
-	pr_devel("close() called. Major: %u, Minor: %u\n",
+	pr_debug("release() called. Major: %u, Minor: %u\n",
 		imajor(inode), iminor(inode));
 
 	return 0;
@@ -38,14 +44,52 @@ static ssize_t misc_chardrv_read(struct file *filep, char __user *buf,
 {
 	unsigned long num_bytes_read = 0;
 
-	if (lbuf < rand_num_size)
-		return -EFAULT;
+	pr_debug("read() called. Major: %u, Minor: %u\n",
+		imajor(filep->f_inode), iminor(filep->f_inode));
 
-	get_random_bytes(&rand_num_buf, rand_num_size);
-	num_bytes_read = lbuf - copy_to_user(buf, &rand_num_buf, rand_num_size);
+	rand_num_buf = kzalloc(lbuf, GFP_KERNEL);
+	if (!rand_num_buf)
+		return -ENOMEM;
+
+	get_random_bytes(rand_num_buf, (int)lbuf);
+	num_bytes_read = lbuf - copy_to_user(buf, rand_num_buf, lbuf);
 	*ppos += num_bytes_read;
 
+	kfree(rand_num_buf);
 	return num_bytes_read;
+}
+
+static void misc_chardrv_unlocked_ioctl_usage(unsigned int cmd)
+{
+	pr_err("Unknown command received: 0x%x\n", cmd);
+	pr_debug("Supported ioctl() Commands:\n\t"
+			"MISC_CHAR_DRV_PANIC[0x%x]\n\t", MISC_CHAR_DRV_PANIC);
+}
+
+static long misc_chardrv_unlocked_ioctl(struct file *filep,
+		unsigned int cmd, unsigned long arg)
+{
+	pr_debug("unlocked_ioctl() called. Major: %u, Minor: %u\n",
+		imajor(filep->f_inode), iminor(filep->f_inode));
+
+	if (_IOC_TYPE(cmd) != MISC_CHAR_DRV_BASE) {
+		misc_chardrv_unlocked_ioctl_usage(cmd);
+		return -EINVAL;
+	}
+
+	switch (cmd)
+	{
+	case MISC_CHAR_DRV_PANIC:
+		panic("MISC_CHAR_DRV_PANIC issued! Major: %u, Minor: %u\n",
+			imajor(filep->f_inode), iminor(filep->f_inode));
+		break;
+
+	default:
+		misc_chardrv_unlocked_ioctl_usage(cmd);
+		return -EINVAL;
+	}
+
+	return 0;
 }
 
 static const struct file_operations misc_chardrv_ops = {
@@ -53,6 +97,7 @@ static const struct file_operations misc_chardrv_ops = {
 	.open = misc_chardrv_open,
 	.release = misc_chardrv_release,
 	.read = misc_chardrv_read,
+	.unlocked_ioctl = misc_chardrv_unlocked_ioctl,
 };
 
 static struct miscdevice misc_chardrv = {
