801ecbee13aab8d347f30e9f4b340356a9446a60
Udayan Prabir Sinha <udayan.sinha@outlook.com>
2026-03-17 10:55:35 +0100

    nvim: Add neo-tree
    Signed-off-by: Udayan Prabir Sinha <udayan.sinha@outlook.com>

--- nvim/lua/plugins/misc.lua
+++ nvim/lua/plugins/misc.lua
@@ -14,5 +14,15 @@ return {
 		-- use opts = {} for passing setup options
 		-- this is equivalent to setup({}) function
 	},
+	{
+		-- filesystem tree
+		"nvim-neo-tree/neo-tree.nvim",
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+			"MunifTanjim/nui.nvim",
+			"nvim-tree/nvim-web-devicons", -- optional, but recommended
+		},
+		lazy = false, -- neo-tree will lazily load itself
+	},
 }
 
