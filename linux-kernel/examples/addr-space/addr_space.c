a67623418d81bc3f11a018c18050e471433b9d42
Udayan Prabir Sinha <udayan.sinha@outlook.com>
2026-05-14 10:10:44 +0200

    linux-kernel: Update address space handling example
    Signed-off-by: Udayan Prabir Sinha <udayan.sinha@outlook.com>

--- linux-kernel/examples/addr-space/addr_space.c
+++ linux-kernel/examples/addr-space/addr_space.c
@@ -9,49 +9,65 @@
 #include <linux/container_of.h>	/* container_of() utils */
 #include <linux/sched.h>	/* scheduler APIs */
 #include <linux/mm.h>		/* address space utils */
+#include <linux/types.h>	/* type utils */
+#include <linux/moduleparam.h>	/* module parameter APIs */
 
-static void log_current_task_addr_space(void)
+static pid_t taskpid = 0;
+module_param(taskpid, int, 0);
+MODULE_PARM_DESC(taskpid, "User-space task PID (TGID) whose address space info is to be inspected");
+
+static void do_log_task_addr_space(struct mm_struct *mm)
 {
-	struct task_struct *last_task = NULL;
-	struct mm_struct *mm_ref = NULL;
 	struct vm_area_struct *vma = NULL;
+	VMA_ITERATOR(vmi, mm, 0);
 
-	if (current->mm) {
-		pr_info("Current task is a user-space task: %s\n", current->comm);
-		mm_ref = current->mm;
-	} else {
-		last_task = container_of(&current->active_mm, struct task_struct, mm);
-		pr_info("Current task is a kthread, using last user-space task as reference: %s\n",
-				last_task->comm);
-		mm_ref = current->active_mm;
-	}
-
-	if(!mm_ref) {
-		pr_info("Could not retrieve an address space to inspect\n");
-		return;
-	}
-
-	VMA_ITERATOR(vmi, mm_ref, 0);
-
-	pr_info("Size of virtual address space: %lu\n", mm_ref->task_size);
-	pr_info("PGD: %p\n", mm_ref->pgd);
-	pr_info("Threads: %u\n", atomic_read(&mm_ref->mm_users));
-	pr_info("Number of memory regions: %d\n", mm_ref->map_count);
-	pr_info("Largest RSS usage value: %lu\n", mm_ref->hiwater_rss);
-	pr_info("Largest VM usage value: %lu\n", mm_ref->hiwater_vm);
-	pr_info("Total pages mapped: %lu\n", mm_ref->total_vm);
-	pr_info("Page table size: %lu\n", atomic_long_read(&mm_ref->pgtables_bytes));
+	pr_info("Size of virtual address space: %lu\n", mm->task_size);
+	pr_info("PGD: %p\n", mm->pgd);
+	pr_info("Threads: %u\n", atomic_read(&mm->mm_users));
+	pr_info("Number of memory regions: %d\n", mm->map_count);
+	pr_info("Largest RSS usage value: %lu\n", mm->hiwater_rss);
+	pr_info("Largest VM usage value: %lu\n", mm->hiwater_vm);
+	pr_info("Total pages mapped: %lu\n", mm->total_vm);
+	pr_info("Page table size: %lu\n", atomic_long_read(&mm->pgtables_bytes));
 
 	pr_info("Memory Regions: \n");
 	for_each_vma(vmi, vma) {
-		pr_info("Start Addr: %lu, End Addr: %lu\n",
-			vma->vm_start, vma->vm_end);
+		pr_info("Start Addr: 0x%lx, End Addr: 0x%lx, Flags: 0x%lx\n",
+			vma->vm_start, vma->vm_end, vma->vm_flags);
 	}
+}
+
+static void log_task_addr_space(void)
+{
+	struct task_struct *p = NULL;
+	struct mm_struct *mm = NULL;
+
+	if (!taskpid) {
+		pr_info("Cannot use invalid PID %d for address space inspection\n",
+				taskpid);
+		return;
+	}
+
+	for_each_process(p) {
+		if (p->tgid == taskpid) {
+			pr_info("Attempting address space inspection for %s[%d]\n",
+					p->comm, p->tgid);
+			mm = p->mm;
+			break;
+		}
+	}
+
+	if(!mm) {
+		pr_info("PID does not exist or belongs to kthread\n");
+		return;
+	}
+
+	do_log_task_addr_space(mm);
 }
 
 static int __init addr_space_init(void)
 {
-	log_current_task_addr_space();
+	log_task_addr_space();
 	return 0;
 }
 
